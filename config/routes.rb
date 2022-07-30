0967d7639179b51c9b3d4046fc9747ccea510d3e
Ye Zhu <dellazhuye@gmail.com>
2022-07-30 15:04:35 +0800

    move the review form to restaurant show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   resources :reviews, only: [:destroy]
 end
