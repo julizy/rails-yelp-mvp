ed96d8928b7decd9427b3706b9cb6f9cceaa8d1c
Ye Zhu <dellazhuye@gmail.com>
2022-08-02 01:42:16 +0800

    removed the redundent action #new in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, only: %i[new create]
-
-  def new
-    @review = Review.new
-  end
+  before_action :set_restaurant, only: %i[create]
 
   def create
     @review = Review.new(review_params)
